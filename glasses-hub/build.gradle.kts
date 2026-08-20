07589f480a68820dfda8eb9a29dd86045312c882
Anezium <anezium@gmail.com>
2026-08-20 02:12:33 +0200

    Bump the glasses hub alongside Nexus 1.4.5

--- glasses-hub/build.gradle.kts
+++ glasses-hub/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "com.anezium.rokidbus.glasses"
         minSdk = 31
         targetSdk = 32
-        versionCode = 10404
-        versionName = "1.4.4"
+        versionCode = 10405
+        versionName = "1.4.5"
     }
 
     buildTypes {
